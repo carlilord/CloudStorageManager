f010a1f50271c46dddc08b6d50ec9f537ca18246
Nico Engetsberger <nico@inext.at>
2015-09-23 22:42:16 +0200

    #commit

--- CloudManagemerUI/OneDrive/OneDrive.cs
+++ CloudManagemerUI/OneDrive/OneDrive.cs
@@ -12,7 +12,7 @@ namespace CloudManagerUI
     class OneDriveClass
     {
         public delegate void LoginFinishedDelegate(AuthResult result);
-        public event LoginFinishedDelegate loginFinished;                       //#commitz
+        public event LoginFinishedDelegate loginFinished;                       //#commitzz
 
         public delegate void LogoutFinishedDelegate();
         public event LogoutFinishedDelegate logoutFinished;
